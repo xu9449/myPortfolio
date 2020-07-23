890530d6d9994864e7924b3ecaff127a8d1b7d73
xu9449 <xu9449@gmail.com>
2020-07-23 14:48:05 -0400

    push myimgage and recently changes

---
+++ myportfolio/src/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import Main from './components/main';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import {Link} from 'react-router-dom';
+
+class App extends Component{
+  render(){
+  return (
+        <div className="demo-big-content">
+            <Layout>
+                <Header className = "header-color" title="Sean Xu" scroll>
+                    <Navigation>
+                        <Link to="/resume">Resume</Link>
+                        <Link to="/aboutme">About Me</Link>
+                        <Link to="/projects">Projects</Link>
+                        <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Header>
+                <Drawer title="Title">
+                    <Navigation>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>
+                    </Navigation>
+                </Drawer>
+                <Content>
+                    <div className="page-content" />
+                    <Main/>
+                </Content>
+            </Layout>
+
+        </div>
+
+  );
+}
+}
+
+export default App;
